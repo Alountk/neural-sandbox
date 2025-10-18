c6d47e55581e36f7e5af84b23688b55eb7bdc201
Raul Marchan Catala <alountk@gmail.com>
2025-10-19 01:43:59 +0200

    feat: useIsMountedDemo implementation

--- playground/src/hooks/hooks.ts
+++ playground/src/hooks/hooks.ts
@@ -1,3 +1,4 @@
 // Re-exportación de todos los hooks personalizados para facilitar la importación
 export { useUpdateEffect } from './useUpdateEffect';
 export { useClickOutside } from './useClickOutside';
+export { useIsMounted } from './useIsMounted';
