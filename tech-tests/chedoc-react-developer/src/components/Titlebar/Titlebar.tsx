a21f3ab4ffab958a8a12b0c0b36866855645bbfb
Raul Marchan Catala <alountk@gmail.com>
2025-11-16 20:57:48 +0100

    feat: create titlebar component

---
+++ tech-tests/chedoc-react-developer/src/components/Titlebar/Titlebar.tsx
@@ -0,0 +1,9 @@
+import styles from './Titlebar.module.css'
+
+export default function Titlebar() {
+    return (
+        <header className={styles.header}>
+            Titlebar
+        </header>
+    )
+}
