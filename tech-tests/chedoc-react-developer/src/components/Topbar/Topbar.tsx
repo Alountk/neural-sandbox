dba1f72e53c5c30c6d0904e2feec37951568d8a6
Raul Marchan Catala <alountk@gmail.com>
2025-11-16 21:41:10 +0100

    feat: more styles

--- tech-tests/chedoc-react-developer/src/components/Topbar/Topbar.tsx
+++ tech-tests/chedoc-react-developer/src/components/Topbar/Topbar.tsx
@@ -3,7 +3,7 @@ import styles from './Topbar.module.css'
 export default function Topbar() {
     return (
         <nav className={styles.topbar}>
-            Topbar
+            DEMO Streaming
         </nav>
     )
 }
--- tech-tests/chedoc-react-developer/src/pages/Home/Home.tsx
+++ tech-tests/chedoc-react-developer/src/pages/Home/Home.tsx
@@ -1,7 +1,21 @@
+import styles from "./Home.module.css";
+
+const SECTIONS = {
+  SHOWS: "shows",
+  MOVIES: "movies",
+};
+
 export default function Home() {
-    return (
-        <div>
-            Home Page
+  return (
+    <section className={styles["home-section"]}>
+      {Object.values(SECTIONS).map((section) => (
+        <div key={section}>
+          <div className={styles["home-card"]}>
+            {section}
+          </div>
+          <p className={styles["home-description"]}>{`Popular ${section}`}</p>
         </div>
-    )
+      ))}
+    </section>
+  );
 }
